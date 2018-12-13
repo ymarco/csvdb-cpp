70c5b05dd224617a6f093bd1101fa8fca245ce97
Yoav Marco <yoavm448@gmail.com>
2018-12-13 16:43:03 +0200

    created tokenizer and empty parser

---
+++ Tokenizer.cpp
@@ -0,0 +1,166 @@
+#include "Tokenizer.h"
+//#include <regex>
+
+bool is_in_str_array(std::string* a, size_t size, std::string& s){
+    for(size_t i=0; i<size; i++){
+        if(a[i] == s)
+            return true;
+    }
+    return false;
+}
+bool is_alphabetic_or_underscore(char x){
+    if(isalpha(x) || x==*"_")
+        return true;
+    return false;
+}
+bool is_digit_or_dot_or_pm(char x){
+    if(isdigit(x) || x==*"." || x==*"+" || x==*"-")
+        return true;
+    return false;
+}
+
+
+Tokenizer::Tokenizer(std::string& text): _text_len(text.size()), _text(text){}
+
+std::pair<char, std::string> Tokenizer::NextToken(){
+    std::string token_val;
+
+    __skip_wspace();
+    __skip_comment();
+    __skip_wspace();
+    // token checking
+    if(__eof()){
+        return {EOF, ""};
+    }
+
+    if(is_alphabetic_or_underscore(_cur())){
+        return _get_identifier_or_keyword();
+    }
+
+    if(_cur() == *"\""){
+        return _get_lit_str();
+    }
+
+    if( is_digit_or_dot_or_pm(_cur()) ){
+        return _get_lit_num();
+    }
+    /* if we got here, it means that our current token is not
+       an end-of-file, keyword, identifier, or literal
+       so it must be an operator (or an error)
+    */
+    return _get_operator();
+    
+   
+     
+}
+
+// returns the identifier or keyword in current text position
+std::pair<char, std::string> Tokenizer::_get_identifier_or_keyword(){
+    std::string token_val;
+    while( !__eof() && ( is_alphabetic_or_underscore(_cur()) || isdigit(_cur()) ) ){
+        token_val.push_back(_cur());
+        _curser ++;
+    }
+    if(is_in_str_array(_keywords,34 ,token_val))
+        return {t_KEYWORD, token_val};
+    return {t_IDENTIFIER, token_val};
+}
+
+std::pair<char, std::string> Tokenizer::_get_lit_str(){
+    size_t start_pos = _curser; //used for error printing
+    std::string token_val;
+    if(_cur()!=*"\"")
+        return {t_ERROR, "_get_lit_str was called, but there wasnt a \" in the _cur"};
+    _curser ++; // now we are inside the str lit
+    while(!__eof()){
+        if(_cur()==*"\""){ // found the closing "
+            _curser ++; // going past the " and exiting the str lit
+            return {t_LIT_STR, token_val};
+        }
+        token_val.push_back(_cur());
+        _curser ++;
+    }
+    //if we got here it means that we reached eof without finding the closing "
+    std::string err_msg = "did not find the closing \" that was opened in char #";
+    err_msg += start_pos;
+    return {t_ERROR, err_msg};
+}
+
+std::pair<char, std::string> Tokenizer::_get_lit_num(){
+    size_t start_pos = _curser; //used for error printing
+    std::string token_val;
+    if(!is_digit_or_dot_or_pm(_cur()))
+        return {t_ERROR, "_get_lit_num was called, but the \"number\" didnt start with digit,.,+,- in the _cur"};
+    bool is_dotted = false;
+    while(!__eof()){
+        if( isdigit(_cur()) ){
+            token_val.push_back(_cur());
+        }else if(_cur() == *"."){
+            if(is_dotted){              
+                return {t_ERROR, "_get_lit_num found 2 dots without exponent"};
+            }else{
+                is_dotted = true;
+                token_val.push_back(_cur());
+            }
+        }else if(isspace(_cur())){ // end of number
+            return {t_LIT_NUM, token_val};
+        }else{
+            return {t_ERROR, "a thing started with a number and than changed into somethng else invalid"};
+        }
+        _curser ++;
+    }
+    // if we got here it means we reached eof and the numbe hasnt ended
+    return {t_LIT_NUM, token_val};
+}
+
+std::pair<char, std::string> Tokenizer::_get_operator(){
+    std::string token_val(1, _cur());
+    _curser ++;
+    if(is_in_str_array(_operators, 11, token_val)){
+        return {t_OPERATOR, token_val};
+    }
+    if(_text_len = (_curser+1)){//file ends on next char, meaning theres no place for a second operator char
+        return {t_ERROR, "type not found"};
+    }
+    //now we know that there IS one more char, and the combination could
+    token_val.push_back(_cur());
+    _curser ++;
+    if(is_in_str_array(_operators, 11, token_val)){
+        return {t_OPERATOR, token_val};
+    }
+    // we ran out of options... 
+    return {t_ERROR, "type not found"};
+}
+
+void Tokenizer::__skip_wspace(){
+    while(!__eof() && isspace(_cur())){
+        _curser++;
+    };
+}
+
+void Tokenizer::__skip_comment(){
+    if((_curser+1)==_text_len){
+        //there is 1 char left before eof, so there is no chance for a comment
+        return; 
+    }
+    if(_text.substr(_curser, 2)=="--"){
+        __skip_till_eol();
+    }
+}
+
+void Tokenizer::__skip_till_eol(){
+    while(!(__eof())){
+        if(_cur() == *"\n"){
+            _curser ++; // going past the \n
+            return;
+        }
+        _curser ++;
+    }
+
+}
+
+bool Tokenizer::__eof(){
+    if(_text_len == _curser)
+        return true;
+    return false;
+}
---
+++ Tokenizer.h
@@ -0,0 +1,100 @@
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+#include <string>
+#include <utility>
+
+#define t_ERROR      -1
+#define t_EOF 	    0
+#define t_KEYWORD 	1
+#define t_IDENTIFIER 	2
+#define t_LIT_STR 	3
+#define t_LIT_NUM 	4
+#define t_OPERATOR 	5
+
+bool is_in_str_array(std::string* a,size_t size, std::string& s);
+bool is_alphabetic_or_underscore(char x);
+bool is_digit_or_dot_or_pm(char x);
+
+
+std::string _keywords[34] = {
+        "select",
+        "from",
+        "where",
+        "avg",
+        "sum",
+        "min",
+        "max",
+        "load",
+        "drop",
+        "order",
+        "by",
+        "group",
+        "into",
+        "outfile",
+        "as",
+        "having",
+        "data",
+        "infile",
+        "table",
+        "ignore",
+        "lines",
+        "null",
+        "int",
+        "float",
+        "varchar",
+        "timestamp",
+        "desc",
+        "asc",
+        "and",
+        "or",
+        "not",
+        "create",
+        "if",
+        "exists",
+    };
+
+std::string _operators[11] = {
+        ",",
+        "(",
+        ")",
+        "<",
+        "<=",
+        "<>",
+        "=",
+        ">=",
+        ">",
+        ";",
+        "*",
+};
+
+
+
+class Tokenizer{
+private:
+    std::string _text;
+    size_t _text_len; 
+    size_t _curser; // index to _text
+    /* implement later:
+    size_t _cur_line_start_index;
+    size_t _line_number;
+    */
+
+    inline char _cur(){return _text[_curser];};
+    std::pair<char, std::string> _get_identifier_or_keyword();
+    std::pair<char, std::string> _get_lit_str();
+    std::pair<char, std::string> _get_lit_num(); // note that this returns a string containing the num
+    std::pair<char, std::string> _get_operator();
+    void __skip_wspace();
+    void __skip_comment();
+    void __skip_till_eol();
+    bool __eof();
+
+
+public:
+    Tokenizer(std::string& text);
+    std::pair<char, std::string> NextToken(); //returns token kind, token value
+
+};
+
+#endif
