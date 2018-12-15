1be80c89478ff0f32052d6ec158b2ba9f68b3249
אופק נרינסקי <ofek2608@gmail.com>
2018-12-15 13:27:27 +0000

    TreeNode.cpp created online with Bitbucket

---
+++ TreeNode.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class TreeNode
+{
+private:
+	std::pair<char, std::string> thisElement;
+	TreeNode* NextElements;
+	int NextElementsAmount;
+
+public:
+	TreeNode(std::pair<char, std::string> thisElement, TreeNode* NextElements, int NextElementsAmount)
+	{
+		this->thisElement = thisElement;
+		this->NextElements = NextElements;
+		this->NextElementsAmount = NextElementsAmount;
+	}
+
+	std::pair<char, std::string> GetElement()
+	{
+		return thisElement;
+	}
+
+	TreeNode* GetNextElements()
+	{
+		return NextElements;
+	}
+
+	int GetNextElementsAmount()
+	{
+		return NextElementsAmount;
+	}
+};
